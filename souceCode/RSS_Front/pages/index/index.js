4e143c50891350dffcd4686e337195880e7e1ba3
zhaoxu <953071759@qq.ccom>
2019-03-24 15:27:15 +0800

    微信小程序前端添加了推荐页面的轮播和点击一级菜单时隐藏推荐

--- souceCode/RSS_Front/pages/index/index.js
+++ souceCode/RSS_Front/pages/index/index.js
@@ -2,14 +2,19 @@
 //获取应用实例
 const app = getApp()
 
+var url1 = "https://baike.baidu.com/pic/%E6%B6%AE%E7%BE%8A%E8%82%89/476036/0/3801213fb80e7bec3d233a53252eb9389a506b82?fr=lemma&ct=single#aid=0&pic=3801213fb80e7bec3d233a53252eb9389a506b82";
+
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    choose_status : 0,
+
     classfiyList: [
-      { id: 1, name: "热煮", idenity_name: "food_hot" },
+      { id: 1, name: "推荐", idenity_name: "recommend" },
       { id: 2, name: "精品凉菜", idenity_name: "food_cool" },
       { id: 3, name: "下酒菜", idenity_name: "food_auxiliary" },
       { id: 4, name: "特色菜", idenity_name: "food_feature" },
@@ -19,6 +24,39 @@ Page({
       { id: 8, name: "果汁", idenity_name: "food_juice" },
       { id: 9, name: "山珍海味", idenity_name: "food_treasure" },
     ],
+
+    recommend_circly:[//轮播展示的推荐菜的列表
+      { 
+        pic_url: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2903730956,4222895621&fm=27&gp=0.jpg',
+        name : "test_1"},
+      {
+        pic_url: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2903730956,4222895621&fm=27&gp=0.jpg',
+        name: "test_2"
+      },
+      {
+        pic_url: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2903730956,4222895621&fm=27&gp=0.jpg',
+        name: "test_3"
+      }
+    ],
+
+    recommend_list: [
+      {
+        pic_url: url1,
+        name: "test_1"
+      },
+      {
+        pic_url: url1,
+        name: "test_2"
+      },
+      {
+        pic_url: url1,
+        name: "test_3"
+      },
+      {
+        pic_url: url1,
+        name: "test_4"
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -61,6 +99,13 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+  /* 点击左侧一级菜单，调整选中状态 */
+  chooseMenuItem : function(e){
+    this.setData({
+      choose_status: e.currentTarget.dataset.num
     })
   }
   
