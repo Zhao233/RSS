b6ac43ad6d6ad7aa6f49ab946b593217a0f4a077
zhaoxu <953071759@qq.com>
2019-03-31 18:23:14 +0800

    为管理员controller下的food添加了CRUD接口

--- souceCode/RSS/src/main/java/com/example/demo/controller/adminController/foodController/AdminFoodController.java
+++ souceCode/RSS/src/main/java/com/example/demo/controller/adminController/foodController/AdminFoodController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,21 +16,49 @@ import java.util.Map;
 @Controller
 @RequestMapping("/admin/food")
 public class AdminFoodController {
+
+    /**
+     * 根据menuId获取一级菜单下所有的菜品
+     * */
     @ResponseBody
     @RequestMapping(value="/getFoodsByMenuId")
-    public Map<String, Object> getFoodsByMenuId(@RequestParam(name = "menuId", required = true) long menuId){//获取所有一级菜单
+    public Map<String, Object> getFoodsByMenuId(@RequestParam(name = "menuId", required = true) long menuId,
+                                                @RequestParam(name = "search") String search,
+                                                @RequestParam(name = "offset") int offset,
+                                                @RequestParam(name = "limit") int limit){
         Map<String, Object> map = new HashMap();
 
         map.put("response", "getFoodsByMenuId");
         return map;
     }
 
-    @PostMapping(value = "/add")
-    public Map<String, Object> addFood(@RequestParam(name = "menuId") long menuId,
+    @ResponseBody
+    @RequestMapping(value = "/add")
+    public Map<String, Object> addFood(MultipartHttpServletRequest request,
+                                       @RequestParam(name = "menuId") long menuId,
                                        @RequestParam(name = "name") String name,
                                        @RequestParam(name = "role") int role,
-                                       @RequestParam(name = "styles") String styles,
-                                       @RequestParam(name = "picture") MultipartFile picture){
+                                       @RequestParam(name = "styles") String styles){
+        Map<String, Object> map = new HashMap();
+
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/update")
+    public Map<String, Object> updateFood(MultipartHttpServletRequest request,
+                                       @RequestParam(name = "menuId") long menuId,
+                                       @RequestParam(name = "name") String name,
+                                       @RequestParam(name = "role") int role,
+                                       @RequestParam(name = "styles") String styles){
+        Map<String, Object> map = new HashMap();
+
+        return null;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/delete")
+    public Map<String, Object> deleteFood(@RequestParam(name = "foodId") String foodId){
         Map<String, Object> map = new HashMap();
 
         return null;
