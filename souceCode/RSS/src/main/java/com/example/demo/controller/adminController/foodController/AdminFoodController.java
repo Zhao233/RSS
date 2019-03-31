30c746dc44a97f621b36e1e42bfc470a004090ef
zhaoxu <953071759@qq.com>
2019-03-31 18:14:47 +0800

    增加了cookerRole类

--- souceCode/RSS/src/main/java/com/example/demo/controller/adminController/foodController/AdminFoodController.java
+++ souceCode/RSS/src/main/java/com/example/demo/controller/adminController/foodController/AdminFoodController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller.adminController.foodController;
 
 
+import com.example.demo.domain.foodInfo.Food;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,5 +22,16 @@ public class AdminFoodController {
 
         map.put("response", "getFoodsByMenuId");
         return map;
+    }
+
+    @PostMapping(value = "/add")
+    public Map<String, Object> addFood(@RequestParam(name = "menuId") long menuId,
+                                       @RequestParam(name = "name") String name,
+                                       @RequestParam(name = "role") int role,
+                                       @RequestParam(name = "styles") String styles,
+                                       @RequestParam(name = "picture") MultipartFile picture){
+        Map<String, Object> map = new HashMap();
+
+        return null;
     }
 }
--- souceCode/RSS/src/main/java/com/example/demo/domain/user/CookerRole.java
+++ souceCode/RSS/src/main/java/com/example/demo/domain/user/CookerRole.java
@@ -1,4 +1,34 @@
 package com.example.demo.domain.user;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Component
+@Table(name = "cooker_role")
+@Data
 public class CookerRole {
+    /**
+     * 数据库id
+     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Column(name = "create_time")
+    private Timestamp createTime;
+
+    @Column(name = "update_time")
+    private Timestamp updateTime;
+
+
 }
