938e6d26666992da15ccbfa0368633b7d7d9599d
zhaoxu <953071759@qq.com>
2019-03-31 16:04:46 +0800

    添加工具类，通过字符串转出id(Long)列表

---
+++ souceCode/RSS/src/main/java/com/example/demo/util/StringTranslater.java
@@ -0,0 +1,22 @@
+package com.example.demo.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+public class StringTranslater<T> {//T:目标数据类型
+
+    public List<Long> getListFromString(String string, int type){//type:0 转换为long
+        List<Long> list = new LinkedList<Long>();
+
+        String[] strings = string.split("_");
+
+        for(String index : strings){
+            list.add(Long.parseLong(index));
+        }
+
+        return list;
+    }
+}
