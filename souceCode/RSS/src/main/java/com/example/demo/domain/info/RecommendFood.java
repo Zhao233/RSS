4e26242ad262932d7eb7c1af0df9bb8350e0df17
zhaoxu <9503071759@qq.com>
2019-04-07 18:20:25 +0800

    添加了推荐菜品(recommendFood)实例以及对应的repository

---
+++ souceCode/RSS/src/main/java/com/example/demo/domain/info/RecommendFood.java
@@ -0,0 +1,29 @@
+package com.example.demo.domain.info;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Component
+@Table(name = "preference_record")
+@Data
+public class RecommendedFood {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "food_id")
+    private long foodID;
+
+    @Column(name = "type")
+    private int type;
+
+    @Column(name = "create_time")
+    private Timestamp createTime;
+
+    @Column(name = "update_time")
+    private Timestamp updateTime;
+}
