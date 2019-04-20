c5e42229a07ad5afbab8c1eda89247228c61fa9b
zhaoxu <9503071759@qq.com>
2019-04-21 00:15:00 +0800

    完成了购物车中，结算金额的功能

--- souceCode/RSS_Front/pages/cart/cart.js
+++ souceCode/RSS_Front/pages/cart/cart.js
@@ -14,71 +14,8 @@ Page({
     result: [],
 
     food_cart : [
-      // {
-      //   food_id : 0,
-      //   name:'test1',
-      //   desc:'desc desc',
-      //   pic_url: url_pic,
-      //   price:20,
-      //   num:1
-      // },
-      // {
-      //   food_id: 1,
-      //   name: 'test2',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 2,
-      //   name: 'test3',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 3,
-      //   name: 'test4',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 4,
-      //   name: 'test5',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 5,
-      //   name: 'test6',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 6,
-      //   name: 'test7',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-      // {
-      //   food_id: 7,
-      //   name: 'test8',
-      //   desc: 'desc desc',
-      //   pic_url: url_pic,
-      //   price: 20,
-      //   num: 1
-      // },
-    ]
+    ],
+    food_accounts:0
   },
 
   getCartList: function () {//获取购物车卡片列表
@@ -95,6 +32,22 @@ Page({
     });
   },
 
+  getAccounts : function(){
+    var account = 0;
+
+    for (var x of app.globalData.cartListRecord) {
+      var temp_item = x[1];
+
+      account+=(temp_item.price*temp_item.num);
+    }
+
+    account*=100;
+
+    this.setData({
+      food_accounts : account
+    })
+  },
+
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -102,15 +55,14 @@ Page({
     })
   },
   onLoad: function () {
-    this.getCartList();
+    
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  onShow : function () {
+    this.getCartList();
+    this.getAccounts();
+
+
   },
 
   onChange(event) {
