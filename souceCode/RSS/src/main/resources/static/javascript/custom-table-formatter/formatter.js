eac522200ed215f856c2ed18340b1bbbaa6296c9
zhaoxu <9503071759@qq.com>
2019-05-14 03:37:16 +0800

    将所有的table的formatt整理为一个合集

---
+++ souceCode/RSS/src/main/resources/static/javascript/custom-table-formatter/formatter.js
@@ -0,0 +1,22 @@
+function _formatte_id(value, row, index) {
+    if(index == 0){
+        contactsNameMap = new Map();
+    }
+
+    contactsNameMap.set(value,row.name);
+
+    return '<input name="visitor_id" type="checkbox" value="' + value + '" />';
+}
+
+
+function _formatte_time(value, row, index) {
+    if(value==undefined){
+        return '-'
+    }
+
+    return getFormattedTime(value);
+}
+
+function _formatte_select_enable(value, row, index) {
+    return $.trim(value) ==$.trim('1') ? "是":"否" ;
+}
